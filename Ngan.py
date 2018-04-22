835c589a0c6ef22f0e902958ae9d69c5b7a58e1b
Nguyen Ngoc To Ngan <ngan.nguyen011093@gmai.com>
2018-04-22 06:20:46 -0700

    this is Ngan

---
+++ Ngan.py
@@ -0,0 +1,16 @@
+print('My name is Ngan')
+
+from __future__ import print_function
+
+import random
+
+GREETINGS = [
+    'Hi, y\'all',
+    'G\'day, mate!',
+]
+
+def greeting():
+    return random.choice(GREETINGS)
+
+if __name__ == '__main__':
+print(greeting())
